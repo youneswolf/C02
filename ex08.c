bc35b2ac7506197d4e400ed6ac139ac3c4eb2bf0
youneswolf <younesbellakrid@gmail.com>
2023-04-28 16:54:16 +0100

    ex08 strlowcase

---
+++ ex08.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+	char	*ft_strlowcase(char *str){
+	    int i=0;
+	    while(*(str+i)!='\0'){
+	        if(*(str+i)>=65 && *(str+i)<90){
+	            str[i]=str[i]+32;
+	        }
+	        i++;
+	    }
+	    
+	   return (str); 
+	}
+   	
+   
+int main(){
+    char r[]="TEST";
+    printf("%s",ft_strlowcase(r));
+    return 0;
+}
